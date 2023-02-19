7c66a608222432ebb4f83c8e09102daed987abea
DmelladoH <mellado.m2@gmail.com>
2023-02-19 19:01:31 +0100

    finished exercise

--- 2-movieBrowser/src/hooks/useMovies.js
+++ 2-movieBrowser/src/hooks/useMovies.js
@@ -1,17 +1,36 @@
 
 import { getMovies } from '../services/movies'
-import { useState } from 'react'
+import { useRef, useState, useMemo, useCallback } from 'react'
 
-export function useMovies ({ search }) {
+export function useMovies ({ search, sort }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const previusSearch = useRef(search)// a reference that persists between renderings
 
-  const searchMovies = async () => {
-    if (search) {
-      console.log(search)
+  const searchMovies = useCallback(async (search) => {
+    if (search === previusSearch.current) return
+
+    try {
+      setLoading(true)
+      previusSearch.current = search
       const movies = await getMovies({ search })
-      setMovies(movies)
-    }
-  }
 
-  return { searchMovies, movies }
+      setMovies(movies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  const sortedMovies = useMemo(() => { // use memo to memorize values, useCallback for memorize functions
+    return sort
+      ? [...movies].sort((a, b) => b.year - a.year)
+      : movies
+  }, [sort, movies])
+
+  // useMemo, to be used when there is bad performance when doing certain calculations. Do not overuse
+
+  return { searchMovies, movies: sortedMovies, loading, error }
 }
